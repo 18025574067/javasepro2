81cb12a24511a8036d0a0dc405227acd84a51eba
hyb2419805602 <2419805602@qq.com>
2022-04-14 22:12:25 +0800

    1234

---
+++ day01-oop-demo/src/com/itheima/d1_static/StaticFieldDemo1.java
@@ -0,0 +1,11 @@
+package com.itheima.d1_static;
+
+public class StaticFieldDemo1 {
+    public static void main(String[] args) {
+        // 目标：理解static修饰成员变量的作用和访问特点。
+        // 访问：类名.静态成员变量
+        System.out.println(User.onlineNumber);
+
+    }
+
+}
---
+++ day01-oop-demo/src/com/itheima/d1_static/User.java
@@ -0,0 +1,9 @@
+package com.itheima.d1_static;
+
+public class User {
+    /**
+     * 在线人数。
+     * 注意：static修饰的成员变量：静态成员变量，在内存中只有一份，所有人共享。
+     */
+    public static int onlineNumber = 161;
+}
