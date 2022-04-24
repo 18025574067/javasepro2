19cb63c3a8e476af9210eaa1ed31584313a80753
hyb <2419805602@qq.com>
2022-04-24 13:15:38 +0800

    123

--- day01-oop-demo/src/com/itheima/d6_extends_test/Test.java
+++ day01-oop-demo/src/com/itheima/d6_extends_test/Test.java
@@ -4,9 +4,11 @@ public class Test {
     public static void main(String[] args) {
         // 目标：理解继承的设计思想
         Students s = new Students();
-        s.setName("蜘蛛精");
-        s.setAge(999);
-        s.writeInfo();
-        s.queryCourse();
+        s.setName("蜘蛛精"); // 使用父类的
+        s.setAge(999); // 使用父类的
+        System.out.println(s.getName()); // 使用父类
+        System.out.println(s.getAge()); // 使用父类
+        s.writeInfo(); // 子类的方法
+        s.queryCourse(); // 调用父类的。
     }
 }
