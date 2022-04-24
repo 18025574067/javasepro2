5338bb9d7d48c6b7b781ff0805c0bdb19762837f
hyb2419805602 <2419805602@qq.com>
2022-04-24 22:53:17 +0800

    1234

--- day01-oop-demo/src/com/itheima/d6_extends_test/Students.java
+++ day01-oop-demo/src/com/itheima/d6_extends_test/Students.java
@@ -1,4 +1,7 @@
 package com.itheima.d6_extends_test;
 
-public class Students {
+public class Students extends People{
+    public void writeInfo(){
+        System.out.println(getName() + "写下心得。。。");
+    }
 }
