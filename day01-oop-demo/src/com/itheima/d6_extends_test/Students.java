749d25713977d5e30884ca570c2cb111fe550a25
hyb2419805602 <2419805602@qq.com>
2022-04-24 22:48:55 +0800

    1234

--- day01-oop-demo/src/com/itheima/d6_extends_test/Students.java
+++ day01-oop-demo/src/com/itheima/d6_extends_test/Students.java
@@ -1,13 +1,4 @@
 package com.itheima.d6_extends_test;
 
-/**
-    子类
- */
-public class Students extends People {
-    /**
-        独有的行为，填写反馈信息
-     */
-    public void writeInfo(){
-        System.out.println(getName() + "写下了：学习语法，好哈皮。。。");
-    }
+public class Students {
 }
