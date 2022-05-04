bea03d5f63b4f1e40ba10fe001c37eeaf060ed92
hyb2419805602 <2419805602@qq.com>
2022-05-04 23:16:59 +0800

    1234

--- day02-oop-demo/src/com/itheima/d2_modifier/itcast/Zi.java
+++ day02-oop-demo/src/com/itheima/d2_modifier/itcast/Zi.java
@@ -4,12 +4,19 @@ import com.itheima.d2_modifier.Fu;
 
 public class Zi extends Fu {
     public static void main(String[] args) {
-        //
+        // 不同包下的子类。
+
+        // 父类
         Fu f = new Fu();
 //        f.privateMethod();
 //        f.method();
 //        f.protectedMethod();
         f.publicMethod();
+
+        // 子类
+        Zi z = new Zi();
+        z.protectedMethod();
+        z.publicMethod();
     }
 
 }
