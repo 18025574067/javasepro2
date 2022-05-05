471d8aa0ba6e3e7dceb8b87dd368262078f9c441
hyb123 <hyb2419805602@163.com>
2022-05-05 22:42:55 +0800

    12345

--- day02-oop-demo/src/com/itheima/d3_final/Test2.java
+++ day02-oop-demo/src/com/itheima/d3_final/Test2.java
@@ -1,6 +1,18 @@
 package com.itheima.d3_final;
 
 public class Test2 {
+
+    /**
+     二、修饰静态成员变量（修饰也称为：常量）
+     */
+    public static final String schoolName = "黑马";
+
+    /**
+        三、修饰实例成员变量
+     */
+    private final String name = "猪八戒";
+
+
     public static void main(String[] args) {
         // 目标：理解final 修饰变量，总规则：有且只能被赋值一次。
         // java 中变量有两种：
@@ -9,10 +21,15 @@ public class Test2 {
         //          -- 实例成员变量
         //          -- 静态成员变量
 
-        // 局部变量
+        // 一、局部变量
         final double rate = 3.14;
 //        rate = 3.19; // 第二次赋值了。
         buy(0.8);
+
+//        schoolName = "白马"; // 第二次被赋值。
+
+        Test2 t = new Test2();
+        System.out.println(t.name);
 
 
     }
