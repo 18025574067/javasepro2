738f9cb96f1c8a7b703c379f9a78930f80375ebf
hyb123 <hyb2419805602@163.com>
2022-05-05 21:41:22 +0800

    12345

---
+++ day02-oop-demo/src/com/itheima/d3_final/Test.java
@@ -0,0 +1,15 @@
+package com.itheima.d3_final;
+
+public class Test {
+    public static void main(String[] args) {
+        // 目标：记住final的语法。
+        // 1. final 修饰类，类不能被继承、
+
+    }
+}
+
+
+final class Animal{}
+
+
+//class Wolf extends Animal{}  // 报错，final修饰的类不能被继承。
