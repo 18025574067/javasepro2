0993e39b78ad21fe29de2ca2cfeffe303d9100b7
hyb2419805602 <2419805602@qq.com>
2022-05-07 15:43:52 +0800

    1234

--- day02-oop-demo/src/com/itheima/d3_final/Test2.java
+++ day02-oop-demo/src/com/itheima/d3_final/Test2.java
@@ -3,7 +3,7 @@ package com.itheima.d3_final;
 public class Test2 {
 
     /**
-     二、修饰静态成员变量（修饰也称为：常量）
+     二、修饰静态成员变量（public static final修饰也称为：常量）
      */
     public static final String schoolName = "黑马";
 
@@ -14,26 +14,29 @@ public class Test2 {
 
 
     public static void main(String[] args) {
-        // 目标：理解final 修饰变量，总规则：有且只能被赋值一次。
+        // 目标：理解final 修饰变量，总规则：有且只能被赋值一次(理解语法)
         // java 中变量有两种：
         // 1. 局部变量
         // 2. 成员变量
         //          -- 实例成员变量
         //          -- 静态成员变量
 
-        // 一、局部变量
+        // 一、修饰局部变量
         final double rate = 3.14;
 //        rate = 3.19; // 第二次赋值了。
         buy(0.8);
 
-//        schoolName = "白马"; // 第二次被赋值。
+//        schoolName = "黑马程序员"; // 第二次被赋值。
 
         Test2 t = new Test2();
         System.out.println(t.name);
 //        t.name = "天蓬元帅";
 
         // 注意：final 修饰引用类型的变量 ，其指向的地址值不能改变，但其指向对象的内容可以发生改变。
-        Teacher t2 = new Teacher("学习", "授课", "吹吹水");
+        final Teacher t2 = new Teacher("学习，授课，吹吹水");
+//        t2 = null;
+        t2.setHobby("运动");
+        System.out.println(t2.getHobby());
 
 
     }
@@ -45,7 +48,7 @@ public class Test2 {
 class Teacher{
     private String hobby;
 
-    public Teacher(String 学习, String 授课, String 吹吹水) {
+    public Teacher(String hobby) {
     }
 
     public String getHobby() {
