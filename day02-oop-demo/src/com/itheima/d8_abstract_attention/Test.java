ddf94f5a454c81a5933a1af9c2904d728bb28be9
hyb2419805602 <2419805602@qq.com>
2022-05-10 22:53:17 +0800

    1234

--- day02-oop-demo/src/com/itheima/d8_abstract_attention/Test.java
+++ day02-oop-demo/src/com/itheima/d8_abstract_attention/Test.java
@@ -3,7 +3,30 @@ package com.itheima.d8_abstract_attention;
 public class Test {
     public static void main(String[] args) {
         // 目标：理解抽象类的特征和注意事项。
+        // 1. 类有的东西，抽象类都有
+        // 2. 抽象类中可以没有抽象方法，但是抽象方法必须有抽象类
+        // 3. 一个类继承了抽象类，必须重写抽象类的全部抽象方法，否则必须也定义为抽象类，
+    }
+}
 
+//abstract class Animal{
+//
+//}
+
+class Cat extends Animal{
+
+    @Override
+    public void run() {
 
     }
+
+    @Override
+    public void eat() {
+
+    }
+}
+
+abstract class Animal{
+    public abstract void run();
+    public abstract void eat();
 }
