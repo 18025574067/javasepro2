1b76d51abe86478f9c8d3b27eb6cbd0006a4ab93
hyb123 <hyb2419805602@163.com>
2022-05-11 22:41:19 +0800

    12345

---
+++ day02-oop-demo/src/com/itheima/d9_abstract_template/StudentModdle.java
@@ -0,0 +1,10 @@
+package com.itheima.d9_abstract_template;
+
+public class StudentModdle extends Student{
+
+
+    @Override
+    public String writeMain() {
+        return "我的爸爸很牛b的，开车都不看红绿灯，下辈子还要做他的儿子";
+    }
+}
--- day02-oop-demo/src/com/itheima/d9_abstract_template/Test.java
+++ day02-oop-demo/src/com/itheima/d9_abstract_template/Test.java
@@ -3,7 +3,11 @@ package com.itheima.d9_abstract_template;
 public class Test {
     public static void main(String[] args) {
         // 目标：理解模板方法模式的思想和使用方法。
+        StudentModdle s = new StudentModdle();
+        s.write();
 
+        StudentChild s1 = new StudentChild();
+        s1.write();
 
     }
 }
