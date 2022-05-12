6a1714d3116840a0a934cc86e122ad9ebed2c273
hyb2419805602 <2419805602@qq.com>
2022-05-12 22:58:09 +0800

    1234

--- day02-oop-demo/src/com/itheima/d13_interface_jdk8/SportManInter.java
+++ day02-oop-demo/src/com/itheima/d13_interface_jdk8/SportManInter.java
@@ -7,6 +7,7 @@ public interface SportManInter {
         默认方法，接口不能创建对象，这个方法只能过继给实现类，由实现类的对象调用。
      */
     public default void run(){
+        go();
         System.out.println("跑得很快~~~~");
     }
 
@@ -16,7 +17,17 @@ public interface SportManInter {
         接口的静态方法，只能接口名自己调用的。
      */
     public static void inAddr(){
-        System.out.println("我们都在学习JAVA 新增方法的语法，它是JAVA 源码会用到的~~~~~");
+
+        System.out.println("我们都在学习JAVA 新增方法的语法，它是JAVA 源码自己会用到的~~~~~");
+    }
+
+    /**
+        3. 私有方法
+        -- JDK 1.9 开始支持的。
+        必须在类内部才能被访问
+     */
+    private void go(){
+        System.out.println("开始跑了～～～～～～～～～～～～");
     }
 }
 
@@ -25,9 +36,9 @@ class PingPongMan implements SportManInter{}
 class Test{
     public static void main(String[] args) {
         PingPongMan p = new PingPongMan();
-        SportManInter.inAddr();
         p.run();
-
-//        PingPongMan.inAddr();
+        SportManInter.inAddr();
+        // p.go();
+        // PingPongMan.inAddr();
     }
 }
