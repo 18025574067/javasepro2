11ac09e72654e24805c4ceaebe25d67276c57fca
hyb2419805602 <2419805602@qq.com>
2022-05-13 23:25:36 +0800

    1234

--- day02-oop-demo/src/com/itheima/d14_interface_attention/Test.java
+++ day02-oop-demo/src/com/itheima/d14_interface_attention/Test.java
@@ -17,6 +17,42 @@ public class Test {
         // 5. 一个接口继承了多个接口，是没有问题的，如果多个接口中存在规范冲突则不能多继承。
     }
 }
+// 5. 一个接口继承了多个接口，是没有问题的，如果多个接口中存在规范冲突则不能多继承。
+interface AAA{
+    void run();
+//    int run();
+}
+interface BBB{
+    void run();
+}
+interface CCC extends AAA, BBB{}
+
+
+
+// 4. 一个类实现了多个接口，多个接口中存在同名的默认方法，不冲突，这个类重写该方法即可。
+interface AA{
+    default void go(){
+        System.out.println("AA");
+    }
+}
+interface BB{
+    default void go(){
+        System.out.println("BB");
+    }
+}
+
+class CC implements AA, BB{
+
+    @Override
+    public void go() {
+        AA.super.go();
+    }
+}
+
+
+
+
+
 // 3. 一个类继承了父类，同时又实现了接口，父类中和接口中有同名方法，默认用父类的。
 interface Food{
     default void eat(){
@@ -28,22 +64,17 @@ class Animal{
         System.out.println("父类动物吃~~");
     }
 }
-class Cat extends Animal implements Food{
-
-}
+class Cat extends Animal implements Food{}
 
 // 2. 一个类实现多个接口，多个接口中有同样的静态方法不冲突。原因是接口的静态方法不能给C 调用，只能A 调用A 的，只能B 调用B 的，
 interface A{
-    public static void test(){
+    static void test(){
         System.out.println("A");
     }
 }
 interface B{
-    public static void test(){
+    static void test(){
         System.out.println("B");
     }
 }
 class C implements A, B{}
-
-
-
---
+++ day03-oop-demo/src/com/itheima/d1_porlamorphic/Test.java
@@ -0,0 +1,10 @@
+package com.itheima.d1_porlamorphic;
+
+/**
+    认识多态，理解多态的形式，和概念。
+ */
+public class Test {
+    public static void main(String[] args) {
+        // 多态
+    }
+}
