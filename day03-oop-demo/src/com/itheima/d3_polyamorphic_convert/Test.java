198ecaa7c954aa39496ecdfa2a9ab47046b0a72e
AA <2419805602@qq.com>
2022-05-14 18:19:33 +0800

    123456

--- day03-oop-demo/src/com/itheima/d3_polyamorphic_convert/Test.java
+++ day03-oop-demo/src/com/itheima/d3_polyamorphic_convert/Test.java
@@ -6,7 +6,12 @@ package com.itheima.d3_polyamorphic_convert;
 public class Test {
     public static void main(String[] args) {
         // 自动类型转换
-
+        Animal a = new Dog();
+        a.run();
         // 强制类型转换。
+        Animal a2 = new Tortoise();
+        a2.run();
+        Tortoise t2 = a2;
+        a2.LayEggs();
     }
 }
