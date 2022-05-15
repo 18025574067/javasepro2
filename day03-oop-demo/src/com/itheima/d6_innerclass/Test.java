3fd8fe3aff2aefa829f433ae49b04843fac77719
hyb2419805602 <2419805602@qq.com>
2022-05-15 22:22:54 +0800

    1234

--- day03-oop-demo/src/com/itheima/d6_innerclass/Test.java
+++ day03-oop-demo/src/com/itheima/d6_innerclass/Test.java
@@ -3,6 +3,7 @@ package com.itheima.d6_innerclass;
 public class Test {
     public static void main(String[] args) {
         Outer.Inner in = new Outer().new Inner();
+        in.setName("张三");
         in.show();
         Outer.Inner.test();
     }
