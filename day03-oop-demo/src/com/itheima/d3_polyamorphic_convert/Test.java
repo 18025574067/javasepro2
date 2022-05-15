5c3b8cf0d80f0ad5830e6a31d4835169eef9988f
AA <2419805602@qq.com>
2022-05-15 15:21:47 +0800

    123456

--- day03-oop-demo/src/com/itheima/d3_polyamorphic_convert/Test.java
+++ day03-oop-demo/src/com/itheima/d3_polyamorphic_convert/Test.java
@@ -12,7 +12,6 @@ public class Test {
         Animal a2 = new Tortoise();
         a2.run();
 
-
         // Dog d = (Dog) a2; // 强制类型转换，编译阶段不报错的（注意：有继承关系和实现关系阶段可以强制转换，没毛病），运行阶段报错。
         if (a2 instanceof Tortoise){
             Tortoise t = (Tortoise) a2; // 从父类类型转换成子类类型，必须强制转换。
@@ -21,6 +20,5 @@ public class Test {
             Dog d = (Dog) a2;
             d.lookDoor();
         }
-
     }
 }
