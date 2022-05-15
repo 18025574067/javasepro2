bfccbe79005142831ae4c1ec792cf7cdf7e15f18
AA <2419805602@qq.com>
2022-05-15 14:13:16 +0800

    123456

--- day03-oop-demo/src/com/itheima/d3_polyamorphic_convert/Tortoise.java
+++ day03-oop-demo/src/com/itheima/d3_polyamorphic_convert/Tortoise.java
@@ -6,7 +6,7 @@ public class Tortoise extends Animal {
         System.out.println("乌龟根本跑不动~~~~~~~~~~");
     }
     /**
-     独有功能：看门
+     独有功能：乌龟下蛋。
      */
     public void layEggs(){
         System.out.println("乌龟会下蛋。。。");
