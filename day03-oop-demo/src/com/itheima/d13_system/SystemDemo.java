3c253e3fb33595ee4be5b6c3bb9123e1b452e92f
hyb2419805602 <2419805602@qq.com>
2022-05-21 20:36:32 +0800

    1234

--- day03-oop-demo/src/com/itheima/d13_system/SystemDemo.java
+++ day03-oop-demo/src/com/itheima/d13_system/SystemDemo.java
@@ -13,14 +13,14 @@ public class SystemDemo {
 //         System.exit(0); // JVM终止。
         // 1s = 1000ms
 
-        // 2. 计算机认为为时间有起源：返回1970-1-1 00:00:00 直到此刻的总的毫秒值，时间毫秒值。
+        // 2. 计算机认为时间有起源：返回1970-1-1 00:00:00 直到此刻的总的毫秒值。
         long time = System.currentTimeMillis();
         System.out.println(time);
 
         // 进行时间的计算，性能分析
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < 100000; i++) {
-//            System.out.println("输出：" + i);
+            System.out.println("输出：" + i);
         }
         long endTime = System.currentTimeMillis();
         System.out.println((endTime-startTime)/1000.0 + "s");
