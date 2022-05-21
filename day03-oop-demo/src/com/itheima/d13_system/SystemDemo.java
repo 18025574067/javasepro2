b5f4b0cec223340dc1e980d415abc32a8f3dc7e3
AA <2419805602@qq.com>
2022-05-21 14:43:07 +0800

    123456

---
+++ day03-oop-demo/src/com/itheima/d13_system/SystemDemo.java
@@ -0,0 +1,17 @@
+package com.itheima.d13_system;
+
+public class SystemDemo {
+    public static void main(String[] args) {
+        // System.exit(0); // JVM终止。
+        // 1s = 1000ms
+
+        // 2. 计算机认为为时间有起源：返回1970-1-1 00:00:00 直到此刻的总的毫秒值，时间毫秒值。
+
+        // 3. 做数组拷贝（了解）
+        /**
+         arraycopy(Object src, int srcPls,
+         Object dest, int destPls,
+         int length)
+         */
+    }
+}
