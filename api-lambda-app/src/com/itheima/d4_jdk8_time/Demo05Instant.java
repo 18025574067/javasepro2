b9c0f7bd3fe4603806aa393b7e3762e5f52bdee2
hyb2419805602 <2419805602@qq.com>
2022-06-02 22:38:50 +0800

    1234

--- api-lambda-app/src/com/itheima/d4_jdk8_time/Demo05Instant.java
+++ api-lambda-app/src/com/itheima/d4_jdk8_time/Demo05Instant.java
@@ -1,11 +1,24 @@
 package com.itheima.d4_jdk8_time;
 
 import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Date;
 
 public class Demo05Instant {
     public static void main(String[] args) {
         // 1. 得到一个Instant 时间戳对象
         Instant instant = Instant.now();
         System.out.println(instant);
+
+        // 2. 系统此刻的时间怎么办？
+        Instant instant1 = Instant.now();
+        System.out.println(instant.atZone(ZoneId.systemDefault()));
+
+        // 3. Instant 和 Date 互转
+        Date date = Date.from(instant);
+        System.out.println(date);
+
+        Instant instant2 = date.toInstant();
+        System.out.println(instant2);
     }
 }
--- api-lambda-app/src/com/itheima/d4_jdk8_time/Demo06DateTimeFormat.java
+++ api-lambda-app/src/com/itheima/d4_jdk8_time/Demo06DateTimeFormat.java
@@ -1,4 +1,31 @@
 package com.itheima.d4_jdk8_time;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Demo06DateTimeFormat {
+    public static void main(String[] args) {
+        // 本地此刻 日期时间 对象
+        LocalDateTime ldt = LocalDateTime.now();
+        System.out.println(ldt);
+
+        // 解析/格式化器
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss EEE a");
+
+        // 正向格式化
+        String ldtStr = ldt.format(dtf);
+        System.out.println(ldtStr);
+
+        // 逆向格式化
+        String dtfStr = dtf.format(ldt);
+        System.out.println(dtfStr);
+
+        // 解析字符串时间
+        DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String dateStr = "2019-10-1 12:14:30";
+
+        // 解析当前字符串时间成为本地日期时间对象
+        LocalDateTime ldt1 = LocalDateTime.parse(dateStr, dtf1);
+        System.out.println(ldt1);
+    }
 }
