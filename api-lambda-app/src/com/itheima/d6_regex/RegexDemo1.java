209029100cf836cc119fe443933a1b6a2bfd3340
hyb2419805602 <2419805602@qq.com>
2022-06-05 22:39:19 +0800

    1234

---
+++ api-lambda-app/src/com/itheima/d6_regex/RegexDemo1.java
@@ -0,0 +1,30 @@
+package com.itheima.d6_regex;
+
+public class RegexDemo1 {
+    public static void main(String[] args) {
+        // 需求：检验QQ号码，必须为全数字 6-20位
+        System.out.println(checkQQ("123221a22"));
+        System.out.println(checkQQ("12322122"));
+
+    }
+    public static boolean checkQQ(String qq){
+        // 1. 判断qq号码的长度是否满足要求
+        if (qq == null || qq.length() < 6 || qq.length() > 20){
+            return false;
+        }
+
+        // 2. 判断qq是否为纯数字，不是返回false
+        // 23432a112
+        for (int i = 0; i < qq.length(); i++) {
+            // 获取每位字符
+            char ch = qq.charAt(i);
+            // 判断这个字符是否不是数字，不是返回false
+            if (ch < '0' || ch > '9'){
+                return false;
+            }
+        }
+        return true; // 肯定合法了。
+
+
+    }
+}
