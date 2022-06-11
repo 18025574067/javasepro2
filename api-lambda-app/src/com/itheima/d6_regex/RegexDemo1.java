d05937b9616648a788308f9799fe6fb73762e1e9
hyb2419805602 <2419805602@qq.com>
2022-06-11 23:39:09 +0800

    1234

--- api-lambda-app/src/com/itheima/d6_regex/RegexDemo1.java
+++ api-lambda-app/src/com/itheima/d6_regex/RegexDemo1.java
@@ -8,7 +8,19 @@ public class RegexDemo1 {
         System.out.println(checkQQ(null));
         System.out.println(checkQQ("123"));
 
+        System.out.println("------------------------------------------------");
+        // 正则初体验
+        System.out.println(checkQQ2("123221a22"));
+        System.out.println(checkQQ2("12322122"));
+        System.out.println(checkQQ2(null));
+        System.out.println(checkQQ2("123"));
+
+
     }
+    public static boolean checkQQ2(String qq){
+        return qq != null && qq.matches("\\d{6,20}");
+    }
+
     public static boolean checkQQ(String qq){
         // 1. 判断qq号码的长度是否满足要求
         if (qq == null || qq.length() < 6 || qq.length() > 20){
