4898ff83de1ea362ba00456c642b2901bd799b7d
hyb2419805602 <2419805602@qq.com>
2022-06-25 23:53:36 +0800

    1234

--- collection-app/src/com/itheima/d4_collection_object/TestDemo.java
+++ collection-app/src/com/itheima/d4_collection_object/TestDemo.java
@@ -5,7 +5,7 @@ public class TestDemo {
         // 1. 定义一个电影类
         // 2. 定义一个集合对象存储3部电影对象
 
-        movies.add(new Movie("《你好，李焕英》", 9.5, "张小斐，贾玲，沈腾，陈赫"));
+//        movies.add(new Movie("《你好，李焕英》", 9.5, "张小斐，贾玲，沈腾，陈赫"));
 
     }
 }
