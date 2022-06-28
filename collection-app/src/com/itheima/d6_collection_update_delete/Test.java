7344121f1e69d43deb2c645dc9cf317551d35aa2
AA <2419805602@qq.com>
2022-06-28 18:08:26 +0800

    123456

--- collection-app/src/com/itheima/d6_collection_update_delete/Test.java
+++ collection-app/src/com/itheima/d6_collection_update_delete/Test.java
@@ -24,23 +24,44 @@ public class Test {
 
         // 需求：删除全部的Java信息。
         // a. 迭代器删除
-        Iterator<String> it = list.iterator();
-        while (it.hasNext()){
-            String ele = it.next();
-            if ("Java".equals(ele)){
-//                list.remove("Java"); // 报错
-                it.remove();
-            }
-        }
-        System.out.println(list);
-
+//        Iterator<String> it = list.iterator();
+//        while (it.hasNext()){
+//            String ele = it.next();
+//            if ("Java".equals(ele)){
+////                list.remove("Java"); // 报错
+//                it.remove(); // 使用迭代器删除元素，可以保证不后移，能够成功遍历并删除元素。
+//            }
+//        }
 
         // b. foreach删除
+//        for (String s : list) {
+//            if ("Java".equals(s)){
+//                list.remove(s);
+//            }
+//        }
 
         // c. lambda删除
+//        list.forEach(s ->{
+//            if ("Java".equals(s)) {
+//                list.remove(s);}
+//            });
 
-        // d. for循环
+        // d. for循环（不会出现异常错误，但会漏删除元素，）
+//        for (int i = 0; i < list.size(); i++) {
+//            String ele = list.get(i);
+//            if ("Java".equals(ele)){
+//                list.remove(ele);
+//                i--;
+//            }
+//        }
+        // 从后往前删。
+        for (int i = list.size()-1; i >= 0; i--) {
+            String ele = list.get(i);
+            if ("Java".equals(ele)){
+                list.remove(ele);
+            }
+        }
 
-
+        System.out.println(list);
     }
 }
