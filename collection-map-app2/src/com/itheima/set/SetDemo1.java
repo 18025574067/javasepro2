e965d7977702bc82427586e6a6e475d297c0157b
AA <2419805602@qq.com>
2022-07-02 17:22:36 +0800

    123456

---
+++ collection-map-app2/src/com/itheima/set/SetDemo1.java
@@ -0,0 +1,24 @@
+package com.itheima.set;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class SetDemo1 {
+    public static void main(String[] args) {
+        // 看看Set系列集合的特点：HashSet LinkedHashSet TreeSet
+
+        Set<String> sets = new HashSet<>(); // 一行经典代码, 无序，不重复，无索引
+//        Set<String> sets = new LinkedHashSet<>(); // 有序，不重复，无索引。
+        sets.add("Java");
+        sets.add("Java");
+        sets.add("MySQL");
+        sets.add("MySQL");
+        sets.add("SpringBoot");
+        sets.add("SpringBoot");
+        sets.add("HTML");
+        sets.add("HTML");
+        System.out.println(sets);
+
+    }
+}
---
+++ collection-map-app2/src/com/itheima/set/SetDemo2.java
@@ -0,0 +1,7 @@
+package com.itheima.set;
+
+public class SetDemo2 {
+    public static void main(String[] args) {
+
+    }
+}
