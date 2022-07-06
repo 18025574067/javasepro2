ddcf52ae6400ac5c25df5c2c5f4393ebdce1573d
hyb2419805602 <2419805602@qq.com>
2022-07-06 22:56:36 +0800

    1234

--- collection-map-app2/src/com/itheima/set/SetDemo3.java
+++ collection-map-app2/src/com/itheima/set/SetDemo3.java
@@ -11,6 +11,17 @@ public class SetDemo3 {
     public static void main(String[] args) {
         Set<Student> sets = new HashSet<>();
 
+        // Set集合去重原理：先判断哈希值，再判断equals.
+        Student s1 = new Student("无恙", 20, '男');
+        Student s2 = new Student("无恙", 20, '男');
+        Student s3 = new Student("周雄", 21, '男');
+
+        sets.add(s1);
+        sets.add(s2);
+        sets.add(s3);
+
+        System.out.println(sets);
+
 
     }
 }
