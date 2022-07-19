1b99da8af7ddbc26b5f0d6dc596eca9a0c735558
hyb2419805602 <2419805602@qq.com>
2022-07-19 23:58:33 +0800

    1234

--- collection-map-app2/src/com/itheima/d1_set/SetDemo5.java
+++ collection-map-app2/src/com/itheima/d1_set/SetDemo5.java
@@ -33,14 +33,22 @@ public class SetDemo5 {
         /**
             方式二：集合自带比较器对象进行规则定制
          */
-        Set<Apple> apples = new TreeSet<>(new Comparator<Apple>() {
-            @Override
-            public int compare(Apple o1, Apple o2) {
-//                return o1.getWeight() - o2.getWeight(); // 升序
-//                return o2.getWeight() - o1.getWeight(); // 降序
-                return Double.compare(o1.getPrice(), o2.getPrice());
-            }
-        });
+//        Set<Apple> apples = new TreeSet<>(new Comparator<Apple>() {
+//            @Override
+//            public int compare(Apple o1, Apple o2) {
+////                return o1.getWeight() - o2.getWeight(); // 升序
+////                return o2.getWeight() - o1.getWeight(); // 降序
+//                // 注意：浮点型建议直接使用Double.compare进行比较。
+//                return Double.compare(o1.getPrice(), o2.getPrice()); // 升序
+////                return Double.compare(o2.getPrice(), o1.getPrice()); // 降序
+////                return o1.getPrice() - o2.getPrice(); // 类型错误。
+//            }
+//        });
+
+        // 简化写法
+        Set<Apple> apples = new TreeSet<>((o1, o2) -> Double.compare(o1.getPrice(), o2.getPrice()) );
+
+
         apples.add(new Apple("红富士", "红色", 9.9, 500));
         apples.add(new Apple("青苹果", "青色", 19.9, 300));
         apples.add(new Apple("绿苹果", "绿色", 29.9, 800));
