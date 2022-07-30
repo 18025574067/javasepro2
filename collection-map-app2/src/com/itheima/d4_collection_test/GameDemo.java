81c6bf9ff00cdc32dfae71de16fb8a91ef3586c6
hyb2419805602 <2419805602@qq.com>
2022-07-30 23:05:06 +0800

    1234

---
+++ collection-map-app2/src/com/itheima/d4_collection_test/GameDemo.java
@@ -0,0 +1,40 @@
+package com.itheima.d4_collection_test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+    目标：斗地主游戏的案例开发。
+
+    业务需求分析：
+        斗地主的做牌，洗牌，发牌，排序（拓展知识），看牌。
+        业务：总共有54张牌。
+        点数："3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2".
+        花色："", "", "", ""
+
+
+
+    大小王："", ""
+    点数分别要组合4种花色，大小王各一张。
+    斗地主：发出51张牌，剩下3张作为底牌。
+ 功能：
+    1.做牌
+    2.洗牌
+    3.定义3个玩家
+    4.发牌
+    5.排序（拓展，了解，作业）
+    6.看牌
+ */
+public class GameDemo {
+    /**
+        1. 定义一个静态的集合存储54张牌
+
+     */
+
+    public static List<Card> allCard = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+
+    }
+}
