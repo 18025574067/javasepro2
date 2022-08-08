8cdca55c8fc0dbd6e606a15a2fd6b4fbe269c693
hyb2419805602 <2419805602@qq.com>
2022-08-08 22:52:12 +0800

    1234

--- collection-map-app2/src/com/itheima/d9_map_impl/LinkedHashMapDemo2.java
+++ collection-map-app2/src/com/itheima/d9_map_impl/LinkedHashMapDemo2.java
@@ -1,24 +1,18 @@
-package com.itheima.d5_map;
+package com.itheima.d9_map_impl;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-/**
-    目标：认识Map体系的特点：按照键无序，无索引，不重复，值不做要求。
- */
-public class MapDemo1 {
+public class LinkedHashMapDemo2 {
     public static void main(String[] args) {
         // 1. 创建一个Map集合对象
-        Map<String, Integer> maps = new HashMap<>(); // 一行经典代码。
-//        Map<String, Integer> maps = new LinkedHashMap<>(); // 一行经典代码。
+        Map<String, Integer> maps = new LinkedHashMap<>(); // 一行经典代码。
         maps.put("鸿星尔克", 3);
         maps.put("Java", 3);
         maps.put("枸杞", 100);
         maps.put("Java", 100); // 会覆盖前面的值。
         maps.put(null, null);
         System.out.println(maps);
-
 
     }
 }
