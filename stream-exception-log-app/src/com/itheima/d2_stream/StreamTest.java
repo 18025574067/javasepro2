08e034f4fc36c73252b8b51f9cd499e6993213e1
AA <2419805602@qq.com>
2022-08-10 16:45:09 +0800

    123456

--- stream-exception-log-app/src/com/itheima/d2_stream/StreamTest.java
+++ stream-exception-log-app/src/com/itheima/d2_stream/StreamTest.java
@@ -1,10 +1,16 @@
 package com.itheima.d2_stream;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
     初步体验Stream流的便捷。
  */
 public class StreamTest {
     public static void main(String[] args) {
-
+        List<String> names = new ArrayList<>();
+        Collections.addAll(names, "张三丰", "周芷若", "赵敏", "张无忌", "张强");
+        System.out.println(names);
     }
 }
