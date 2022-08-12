2b14e4d343e0a086fa3849425eb9f63a5f7882ad
hyb2419805602 <2419805602@qq.com>
2022-08-12 23:54:26 +0800

    1234

--- stream-exception-log-app/src/com/itheima/d2_stream/StreamDemo05.java
+++ stream-exception-log-app/src/com/itheima/d2_stream/StreamDemo05.java
@@ -1,6 +1,7 @@
 package com.itheima.d2_stream;
 
 import java.util.*;
+import java.util.function.IntFunction;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,15 +17,34 @@ public class StreamDemo05 {
         list.add("赵敏");
         list.add("张强");
         list.add("张三丰");
-        System.out.println(list);
+        list.add("张三丰");
 
-        Stream<String> s1 = list.stream().filter(s -> s.startsWith("张"));
-        List<String> zhangList = s1.collect(Collectors.toList());
-        System.out.println(zhangList);
+//        Stream<String> s1 = list.stream().filter(s -> s.startsWith("张"));
+//        List<String> zhangList = s1.collect(Collectors.toList());
+//        System.out.println(zhangList);
+        List<String> list1 = list.stream().toList(); // 得到不可变集合。
+//        list1.add("小明");
+        System.out.println(list1);
 
-        // 流只能使用一次。
-        Set<String> zhangSet = s1.collect(Collectors.toSet());
+        // 注意注意注意：流只能使用一次。
+        Stream<String> s2 = list.stream().filter(s -> s.startsWith("张"));
+        Set<String> zhangSet = s2.collect(Collectors.toSet());
         System.out.println(zhangSet);
+
+        Stream<String> s3 = list.stream().filter(s -> s.startsWith("张"));
+//        Object[] arrs = s3.toArray();
+
+        String[] arrs = s3.toArray(String[]:: new);
+
+//        String[] arrs = s3.toArray(new IntFunction<String[]>() {
+//            @Override
+//            public String[] apply(int value) {
+//                return new String[value];
+//            }
+//        });
+
+
+        System.out.println("Arrays数组内容： " + Arrays.toString(arrs));
 
 
     }
