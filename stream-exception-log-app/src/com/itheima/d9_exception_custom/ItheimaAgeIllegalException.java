6002d522c5e0d0947805c79b50c497db1ea037d8
hyb2419805602 <2419805602@qq.com>
2022-08-17 00:08:02 +0800

    1234

---
+++ stream-exception-log-app/src/com/itheima/d9_exception_custom/ItheimaAgeIllegalException.java
@@ -0,0 +1,10 @@
+package com.itheima.d9_exception_custom;
+
+public class ItheimaAgeIllegalException extends Exception{
+    public ItheimaAgeIllegalException() {
+    }
+
+    public ItheimaAgeIllegalException(String message) {
+        super(message);
+    }
+}
