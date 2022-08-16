94eff4d4e2d620257ec2593f9ea8c272c18579cc
hyb2419805602 <2419805602@qq.com>
2022-08-16 23:26:21 +0800

    1234

--- stream-exception-log-app/src/com/itheima/d8_exception_handle_runtime/Test2.java
+++ stream-exception-log-app/src/com/itheima/d8_exception_handle_runtime/Test2.java
@@ -9,15 +9,19 @@ public class Test2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (true){
-            System.out.println("请您输入合法的价格：");
-            String priceStr = sc.nextLine();
-            // 转换成double类型的价格。
-            double price = Double.valueOf(priceStr);
-            if (price > 0){
-                System.out.println("定价：" + price);
-                break;
-            }else {
-                System.out.println("价格必须是整数：");
+            try {
+                System.out.println("请您输入合法的价格：");
+                String priceStr = sc.nextLine();
+                // 转换成double类型的价格。
+                double price = Double.valueOf(priceStr);
+                if (price > 0){
+                    System.out.println("定价：" + price);
+                    break;
+                }else {
+                    System.out.println("价格必须是整数：");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("用户输入有误，请您输入合法的数值，建议为正数。。。。。。");
             }
         }
     }
