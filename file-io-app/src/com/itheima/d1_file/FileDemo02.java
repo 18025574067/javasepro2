37a94b9d9090b48d8ea8efea4a2fffb5a0289b3e
hyb2419805602 <2419805602@qq.com>
2022-08-25 00:13:56 +0800

    1234

--- file-io-app/src/com/itheima/d1_file/FileDemo02.java
+++ file-io-app/src/com/itheima/d1_file/FileDemo02.java
@@ -15,19 +15,33 @@ public class FileDemo02 {
     public static void main(String[] args) {
         // 1. 绝对路径创建一个文件对象
         File f1 = new File("/home/hyb/文档/code/file");
+        System.out.println("==============================");
+
+        File f2 = new File("file-io-app/src/data.txt");
+
         // a.获取它的绝对路径。
-        System.out.println(f1.getAbsolutePath());
+        System.out.println(f2.getAbsolutePath());
         // b.获取文件定义的时候使用的路径。
-        System.out.println(f1.getPath());
+        System.out.println(f2.getPath());
         // c.获取文件的名称：带后缀。
-        System.out.println(f1.getName());
+        System.out.println(f2.getName());
         // d.获取文件的大小：字节个数
-        System.out.println(f1.length()); // 字节大小。
+        System.out.println(f2.length()); // 字节大小。
         // e.获取文件的最后修改时间。
-        long time = f1.lastModified();
-        System.out.println("最后修改时间为：" + new SimpleDateFormat("yyyy-MM-dd HH:mm:sss").format(time));
+        long time1 = f2.lastModified();
+        System.out.println("最后修改时间为：" + new SimpleDateFormat("yyyy-MM-dd HH:mm:sss").format(time1));
         // f.判断文件对象是文件还是文件夹
         System.out.println(f1.isFile()); // false
         System.out.println(f1.isDirectory()); // true
+
+        File file = new File("/home/");
+        System.out.println(file.isFile()); // false
+        System.out.println(file.isDirectory()); // true
+        System.out.println(file.exists()); // true
+
+        File file1 = new File("/home/aaa");
+        System.out.println(file1.isFile()); // false
+        System.out.println(file1.isDirectory()); // false
+        System.out.println(file1.exists()); // false
     }
 }
--- file-io-app/src/com/itheima/d1_file/FileDemo03.java
+++ file-io-app/src/com/itheima/d1_file/FileDemo03.java
@@ -1,4 +1,42 @@
 package com.itheima.d1_file;
 
+import java.io.File;
+import java.io.IOException;
+
+/**
+    目标：File类的创建和删除的方法：
+    -public boolean createNewFile(): 当且仅当具有该名称的文件尚不存在时，
+            创建一个新的空文件。（几乎不用的，因为以后文件都是自动创建的！）
+    -public boolean delete(): 删除由此File表示的文件或目录。（只能删除空目录）
+    -public boolean mkdir(): 创建由此File表示的目录。（只能创建一级目录）
+    -public boolean mkdirs(): 可以创建多级目录（建议使用的）
+
+ */
 public class FileDemo03 {
+    public static void main(String[] args) throws IOException {
+        File f = new File("file-io-app/src/data.txt");
+        // a. 创建新文件，创建成功返回true, 反之，不需要这个，以后文件写出去的时候都会自动创建
+        System.out.println(f.createNewFile());
+        File f1 = new File("file-io-app/src/data1.txt");
+        System.out.println(f1.createNewFile());
+
+        // b. mkdir创建一级目录
+        File f2 = new File("file-io-app/src/aaa"); // true
+        System.out.println(f2.mkdir());
+
+
+        // c. mkdirs 创建多级目录（重点）
+        File f3 = new File("file-io-app/src/aaa/bbb/ccc"); // false
+        System.out.println(f3.mkdir()); // false
+        System.out.println(f3.mkdirs()); // 支持多级创建。
+
+        // d. 删除文件或者文件夹
+        File f4 = new File("/home/hyb/文档/code/file/Untitled Document");
+        System.out.println(f4.delete());
+
+        // 只能删除空文件夹，不能删除非空文件夹。
+        File f5 = new File("/home/hyb/文档/code/file");
+        System.out.println(f5.delete());
+
+    }
 }
