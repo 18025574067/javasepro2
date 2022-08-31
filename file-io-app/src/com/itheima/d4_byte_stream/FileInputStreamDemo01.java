50925fde19aa0b4095634e6b7b47a5433aabd786
hyb2419805602 <2419805602@qq.com>
2022-08-31 22:56:07 +0800

    1234

--- file-io-app/src/com/itheima/d4_byte_stream/FileInputStreamDemo01.java
+++ file-io-app/src/com/itheima/d4_byte_stream/FileInputStreamDemo01.java
@@ -1,9 +1,6 @@
 package com.itheima.d4_byte_stream;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 
 /**
     -- 构造器
@@ -19,12 +16,30 @@ import java.io.InputStream;
 
  */
 public class FileInputStreamDemo01 {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         // 1. 创建一个文件字节输入流管道连接到源文件接通
 //        InputStream is = new FileInputStream(new File("file-io-app/src/data.txt"));
         InputStream is = new FileInputStream("file-io-app/src/data.txt"); // 简化写法。
 
-        // 2.
+        // 2. 读取一个字节
+//        int b1 = is.read();
+//        System.out.println((char)b1);
+//        //        System.out.println(b1);
+//
+//        int b2 = is.read();
+//        System.out.println((char)b2);
+//
+//        int b3 = is.read();
+//        System.out.println((char)b3);
+//
+//        int b4 = is.read(); // 读取完毕，返回-1.
+//        System.out.println(b4);
 
+        // 3. 使用循环改进
+        // 定义一个变量记录每次读取的字节
+        int b;
+        while ((b = is.read()) != -1){
+            System.out.print((char) b);
+        }
     }
 }
