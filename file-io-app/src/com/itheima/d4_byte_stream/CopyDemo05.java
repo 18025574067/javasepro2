fc40f013bc7c55323ee07b918033acce3745c9a1
AA <2419805602@qq.com>
2022-09-03 17:48:48 +0800

    123456

--- file-io-app/src/com/itheima/d4_byte_stream/CopyDemo05.java
+++ file-io-app/src/com/itheima/d4_byte_stream/CopyDemo05.java
@@ -9,10 +9,12 @@ public class CopyDemo05 {
     public static void main(String[] args) {
         try {
             // 1. 创建一个字节输入流与源文件连接
-            InputStream is = new FileInputStream("/home/hyb/视频/A(视频） 流行抖音MV (50部）/040 李玉刚-新贵妃醉酒.mp4");
+//            InputStream is = new FileInputStream("/home/hyb/视频/A(视频） 流行抖音MV (50部）/040 李玉刚-新贵妃醉酒.mp4");
+            InputStream is = new FileInputStream("E:\\A(视频） 流行抖音MV (50部）\\007 越南版-兄弟想你了 .mp4");
 
             // 2. 创建一个字节输出流与目标文件连接
-            OutputStream os = new FileOutputStream("/home/hyb/桌面/1.avi");
+//            OutputStream os = new FileOutputStream("/home/hyb/桌面/1.mp4");
+            OutputStream os = new FileOutputStream("C:\\Users\\AA\\Desktop\\1.mp4");
 
             // 3. 定义一个字节数组转移数据
             byte[] buffer = new byte[1024];
