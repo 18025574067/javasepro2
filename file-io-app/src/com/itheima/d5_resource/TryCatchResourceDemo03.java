da44b4e491f22f651ab45e4f80cd1280f6a6983e
hyb2419805602 <2419805602@qq.com>
2022-09-04 23:56:45 +0800

    1234

--- file-io-app/src/com/itheima/d5_resource/TryCatchResourceDemo03.java
+++ file-io-app/src/com/itheima/d5_resource/TryCatchResourceDemo03.java
@@ -3,14 +3,14 @@ package com.itheima.d5_resource;
 import java.io.*;
 
 /**
-    目标：JDK9释放资源的方法。
+    目标：JDK9释放资源的方式，了解一下即可。
  */
 public class TryCatchResourceDemo03 {
     public static void main(String[] args) throws Exception {
         // 1. 创建一个字节输入流与源文件连接
-        InputStream is = new FileInputStream("/home/hyb/文档/madia/Track03.mp3");
+        InputStream is = new FileInputStream("file-io-app/src/data1.txt");
         // 2. 创建一个字节输出流与目标文件连接
-        OutputStream os = new FileOutputStream("/home/hyb/桌面/1.mp3");
+        OutputStream os = new FileOutputStream("file-io-app/src/data2.txt");
         try (is; os){
             // 3. 定义一个字节数组转移数据
             byte[] buffer = new byte[1024];
