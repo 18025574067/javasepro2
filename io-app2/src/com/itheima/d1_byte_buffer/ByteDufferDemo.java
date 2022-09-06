3305767421e01ec4b2694ec788fbba449dbfe49b
AA <2419805602@qq.com>
2022-09-06 18:59:22 +0800

    123456

--- io-app2/src/com/itheima/d1_byte_buffer/ByteDufferDemo.java
+++ io-app2/src/com/itheima/d1_byte_buffer/ByteDufferDemo.java
@@ -11,11 +11,11 @@ public class ByteDufferDemo {
         try (
                 // 这里只能放资源对象，用完会自动关闭，自动调用close方法关闭资源（即使出现异常也会做关闭操作）
                 // 1. 创建一个字节输入流与源文件连接
-                InputStream is = new FileInputStream("/home/hyb/图片/Wallpapers/1eff40a7089bd56f718044113ef0f160.jpg");
+                InputStream is = new FileInputStream("io-app2/src/1.jpg");
                 // a. 把原始的字节输入流包装成高级的缓冲字节输入流
                 InputStream bis = new BufferedInputStream(is);
                 // 2. 创建一个字节输出流与目标文件连接
-                OutputStream os = new FileOutputStream("io-app2/src/1.jpg");
+                OutputStream os = new FileOutputStream("io-app2/src/2.jpg");
                 // b. 把字节输出流包装成高级的缓冲字节输出流管道。
                 OutputStream bos = new BufferedOutputStream(os);
         ){
