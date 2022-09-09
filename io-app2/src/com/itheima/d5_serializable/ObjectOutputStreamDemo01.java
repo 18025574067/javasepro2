0a89ad3754326937425e39394a14a411cb3d357f
AA <2419805602@qq.com>
2022-09-09 14:39:33 +0800

    123456

---
+++ io-app2/src/com/itheima/d5_serializable/ObjectOutputStreamDemo01.java
@@ -0,0 +1,10 @@
+package com.itheima.d5_serializable;
+
+/**
+    目标：对象序列化，使用 ObjectOutputStream把内存中的对象存入到磁盘文件中。
+ */
+public class ObjectOutputStreamDemo01 {
+    public static void main(String[] args) {
+
+    }
+}
