45aeac268312a2db9cbca0cc3abe3f250a18d5ad
hyb <2419805602@qq.com>
2022-09-10 15:03:31 +0800

    123

---
+++ io-app2/src/com/itheima/d5_serializable/ObjectInputStreamDemo02.java
@@ -0,0 +1,10 @@
+package com.itheima.d5_serializable;
+
+/**
+    目标：学会对象反序列化，使用对象输入流把文件中的对象数据恢复成内存中的JAVA对象。
+ */
+public class ObjectInputStreamDemo02 {
+    public static void main(String[] args) {
+
+    }
+}
