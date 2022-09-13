51ec84b56369241dfd8ccf842d9fe0c27aa7709f
hyb2419805602 <2419805602@qq.com>
2022-09-13 23:50:26 +0800

    1234

--- io-app2/src/com/itheima/d6_printStream/PrintDemo02.java
+++ io-app2/src/com/itheima/d6_printStream/PrintDemo02.java
@@ -10,7 +10,15 @@ import java.io.PrintStream;
 public class PrintDemo02 {
     public static void main(String[] args) throws Exception {
         // 创建一个打印流对象
-        PrintStream ps = new PrintStream(new FileOutputStream("io-app2/src/1.txt", true)); // 追加数据，在低级管道后面加true.
+        System.out.println("锦瑟无端五十弦");
+        System.out.println("一弦一柱思华年");
 
+        // 改变输出语句的位置（重定向）
+        PrintStream ps = new PrintStream(new FileOutputStream("io-app2/src/log.txt")); // 追加数据，在低级管道后面加true.
+        System.setOut(ps);
+        System.out.println("锦瑟无端五十弦");
+        System.out.println("一弦一柱思华年");
+        System.out.println("庄生晓梦迷蝴蝶");
+        System.out.println("望帝春心托杜鹃");
     }
 }
---
+++ io-app2/src/com/itheima/d7_properties/PropertiesDemo02.java
@@ -0,0 +1,23 @@
+package com.itheima.d7_properties;
+
+/**
+    目标：Properties 读取属性文件中的键值对信息（读取）
+    Properties 的方法：
+        -- public Object setProperty(String key, String value): 保存一对属性
+        -- public String getProperty(String key):使用此属性列表中指定的键搜索属性值
+        -- public Set<String> stringPropertyNames():所有键的名称的集合
+        -- public void store(OutputStream out, String comments):保存数据到属性文件中去
+        -- public synchronized void load(InputStream inStream):加载属性文件的数据到属性中去
+        -- public synchronizes void load(Reader fr):加载属性文件的数据到属性集对象中去
+    小结：
+        属性集对象可以加载读取属性文件中的数据！！
+ */
+
+public class PropertiesDemo02 {
+    public static void main(String[] args) throws Exception {
+        // 需求： 使用Properties把键值对信息存入到属性文件中去。
+
+
+
+    }
+}
