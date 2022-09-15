df5cd00392b8a57cbb7c6eb8196e59725b60f401
AA <2419805602@qq.com>
2022-09-15 17:04:48 +0800

    123456

---
+++ thread-app/src/com/itheima/d1_create/ThreadDemo02.java
@@ -0,0 +1,40 @@
+package com.itheima.d1_create;
+
+/**
+    目标：学会创建线程方式二，理解它的优缺点
+
+ */
+public class ThreadDemo02 {
+    public static void main(String[] args) {
+        // 3. 创建一个线程任务对象
+        Runnable target = new MyRunnable();
+        // 4.
+        new Thread(target).start();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("主线程执行：" + i);
+        }
+    }
+}
+
+/**
+    1. 定义一个线程任务类，继承Runnable接口
+ */
+class MyRunnable implements Runnable{
+    // 2. 重写run方法，定义线程的执行任务的
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("子线程执行：" + i);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
