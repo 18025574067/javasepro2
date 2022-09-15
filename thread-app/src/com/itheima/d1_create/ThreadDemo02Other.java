297d3884675fac3f5afdb6511d161d36ac74cd3f
hyb2419805602 <2419805602@qq.com>
2022-09-15 21:06:07 +0800

    1234

--- thread-app/src/com/itheima/d1_create/ThreadDemo02Other.java
+++ thread-app/src/com/itheima/d1_create/ThreadDemo02Other.java
@@ -6,20 +6,34 @@ package com.itheima.d1_create;
  */
 public class ThreadDemo02Other {
     public static void main(String[] args) {
-        // 3. 创建一个任务对象
         Runnable target = new Runnable() {
             @Override
             public void run() {
                 for (int i = 0; i < 10; i++) {
-                    System.out.println("子线程执行输出：" + i);
+                    System.out.println("子线程1执行输出：" + i);
                 }
             }
         };
-
-        // 4. 把任务对象交给线程对象处理。
         Thread t = new Thread(target);
-        // 5. 启动线程
         t.start();
+
+
+        new Thread( new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("子线程2执行输出：" + i);
+                }
+            }
+        }).start();
+
+
+        new Thread(() -> {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("子线程3执行输出：" + i);
+                }
+        }).start();
+
 
         for (int i = 0; i < 10; i++) {
             System.out.println("主线程执行输出：" + i);
