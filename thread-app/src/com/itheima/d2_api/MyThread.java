bfec79e66a14a56969c2dacda4233405edc65041
hyb2419805602 <2419805602@qq.com>
2022-09-17 00:14:09 +0800

    1234

--- thread-app/src/com/itheima/d2_api/MyThread.java
+++ thread-app/src/com/itheima/d2_api/MyThread.java
@@ -1,6 +1,14 @@
 package com.itheima.d2_api;
 
 public class MyThread extends Thread{
+    public MyThread(String name) {
+        // 为当前线程设置名称，送给父类的有参构造器初始化名称。
+        super(name);
+    }
+
+    public MyThread() {
+    }
+
     @Override
     public void run(){
         for(int i = 0; i < 5; i++){
--- thread-app/src/com/itheima/d2_api/ThreadDemo01.java
+++ thread-app/src/com/itheima/d2_api/ThreadDemo01.java
@@ -5,23 +5,25 @@ package com.itheima.d2_api;
 public class ThreadDemo01 {
     // main方法是由主线程负责调度的
     public static void main(String[] args) {
-        Thread t = new MyThread();
-        t.setName("1号");
+        Thread t = new MyThread("1号");
+//        t.setName("1号");
         t.start();
         System.out.println(t.getName());
 
-        Thread t2 = new MyThread();
-        t2.setName("2号");
+        Thread t2 = new MyThread("2号");
+//        t2.setName("2号");
         t2.start();
         System.out.println(t2.getName());
 
         // 哪个线程执行它，它就得到哪个线程对象（当前线程）
         // 主线程的名称就叫mian
         Thread m = Thread.currentThread();
+        m.setName("最牛的线程");
         System.out.println(m.getName());
 
+
         for(int i = 0; i < 5; i++){
-            System.out.println("main线程输出：" + i);
+            System.out.println(m.getName() + "输出：" + i);
         }
     }
 }
