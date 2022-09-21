0e772b83cd5b2d3cd0756745fad69a1bf67d91d9
hyb2419805602 <2419805602@qq.com>
2022-09-22 00:02:07 +0800

    1234

---
+++ thread-app/src/com/itheima/d8_threadpool/ThreadPoolDemo01.java
@@ -0,0 +1,20 @@
+package com.itheima.d8_threadpool;
+
+import java.util.concurrent.Executor;
+
+/**
+    目标：自定义一个线程池对象，并测试其特性。
+ */
+
+public class ThreadPoolDemo01{
+    public static void main(String[] args){
+        // 1. 创建线程池对象
+
+    }
+
+
+
+}
+
+
+
