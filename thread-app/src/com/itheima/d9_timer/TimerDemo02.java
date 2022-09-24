f53ef1b6c0c3bcad797e49dcf0859e973b3d5c46
hyb2419805602 <2419805602@qq.com>
2022-09-24 21:36:40 +0800

    1234

--- thread-app/src/com/itheima/d9_timer/TimerDemo02.java
+++ thread-app/src/com/itheima/d9_timer/TimerDemo02.java
@@ -1,13 +1,13 @@
 package com.itheima.d9_timer;
 
 import java.util.Date;
-import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
-    目标：Timer定时器的使用和了解。
+    目标：使用ScheduledExecutorService创建定时器的使用和了解，与不干扰。
 */
 
 public class TimerDemo02 {
@@ -15,8 +15,24 @@ public class TimerDemo02 {
         // 1. 创建scheduledExecutorService线程池，做定时器
         ScheduledExecutorService pool = Executors.newScheduledThreadPool(3);
 
-
-
-
+        // 2. 开启定时任务
+        pool.scheduleAtFixedRate(new TimerTask(){
+            @Override
+            public void run(){
+                System.out.println(Thread.currentThread().getName() + "执行输出：AAA" + new Date());
+            }
+        }, 0, 2, TimeUnit.SECONDS);
+        pool.scheduleAtFixedRate(new TimerTask(){
+            @Override
+            public void run(){
+                System.out.println(Thread.currentThread().getName() + "执行输出：BBB" + new Date());
+            }
+        }, 0, 3, TimeUnit.SECONDS);
     }
 }
+
+
+
+
+
+
