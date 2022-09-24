cd362da49c611d2fca91f62fc58a121c7ecee213
hyb <hyb2419805602@163.com>
2022-09-24 18:27:09 +0800

    12345678

---
+++ thread-app/src/com/itheima/d9_timer/TimerDemo02.java
@@ -0,0 +1,16 @@
+package com.itheima.d9_timer;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+    目标：Timer定时器的使用和了解。
+*/
+
+public class TimerDemo02 {
+    public static void main(String[] args) {
+        // 1. 创建scheduledExecutorService线程池，做定时器
+
+    }
+}
