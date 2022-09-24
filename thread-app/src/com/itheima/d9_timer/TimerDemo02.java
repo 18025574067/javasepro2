82e5d6f1f2628ead4cda9d4171abd58e612f7c57
hyb <hyb2419805602@163.com>
2022-09-24 18:29:53 +0800

    12345678

--- thread-app/src/com/itheima/d9_timer/TimerDemo02.java
+++ thread-app/src/com/itheima/d9_timer/TimerDemo02.java
@@ -3,6 +3,8 @@ package com.itheima.d9_timer;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 /**
     目标：Timer定时器的使用和了解。
@@ -11,6 +13,10 @@ import java.util.TimerTask;
 public class TimerDemo02 {
     public static void main(String[] args) {
         // 1. 创建scheduledExecutorService线程池，做定时器
+        ScheduledExecutorService pool = Executors.newScheduledThreadPool(3);
+
+
+
 
     }
 }
