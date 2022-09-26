f7b3ec9e5effb38e0d26d3fbe29ccd161bc484d3
hyb2419805602 <2419805602@qq.com>
2022-09-26 23:36:48 +0800

    1234

---
+++ net-app/src/com/itheima/d4_udp3_broadcast/ServerDemo02.java
@@ -0,0 +1,29 @@
+package com.itheima.d4_udp3_broadcast;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+/**
+    接收端：多发多收
+ */
+public class ServerDemo02 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("========服务端启动========");
+        // 1. 创建服务端对象，注册端口（人）
+        DatagramSocket socket = new DatagramSocket(8080);
+
+        // 2. 创建一个数据包对象接收数据（韭菜盘子）
+        byte[] buffer = new byte[1024*64];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+        while (true) {
+            // 3. 等待接收数据
+            socket.receive(packet);
+
+            // 4. 输出数据
+            int length = packet.getLength();
+            String rs = new String(buffer, 0, length);
+            System.out.println("接收到了来自：" + packet.getAddress() + ", 端口号为：" + packet.getPort() +"的信息：" +rs);
+        }
+    }
+}
