f1486a313dd27e98687c548fe3b2954dd907d93a
AA <2419805602@qq.com>
2022-09-26 14:50:33 +0800

    123456

---
+++ net-app/src/com/itheima/d1_inetAddress/InetAddressDemo01.java
@@ -0,0 +1,27 @@
+package com.itheima.d1_inetAddress;
+
+/**
+    目标：InetAddress类概述（了解）
+        一个该类的对象就代表一个IP地址对象。
+    InetAddress 类成员方法：
+        static InetAddress getLocalHost()
+            * 获得本地主机IP地址对象。
+        static InetAddress getByName(String host)
+            * 根据IP地址字符串主机名获得对应的IP地址对象
+        String getHostName()
+            * 获得主机名
+        String getHostAddress()
+            * 获得IP地址字符串
+ */
+public class InetAddressDemo01 {
+    public static void main(String[] args) {
+        // 1. 获取梧桐地址对象
+
+        // 2. 获取域名IP地址
+
+        // 3. 获取公网IP对象。
+
+        // 4. 判断是否能通： Ping 5s之前测试是否可能
+
+    }
+}
