803ac820605ad1b457c16c5284f97c9d82f14bfa
AA <2419805602@qq.com>
2022-09-26 17:22:55 +0800

    123456

--- net-app/src/com/itheima/d2_udp1/ClientDemo01.java
+++ net-app/src/com/itheima/d2_udp1/ClientDemo01.java
@@ -12,7 +12,7 @@ import java.nio.charset.StandardCharsets;
 public class ClientDemo01 {
     public static void main(String[] args) throws Exception {
         // 1. 创建发送端的对象，发送端自带默认的端口, 可不指定端口。
-        DatagramSocket socket = new DatagramSocket();
+        DatagramSocket socket = new DatagramSocket(8889);
 
         // 2. 创建一个数据包封装数据（韭菜盘子）
         /** public DatagramPacket(byte buf[], int offset, int length,
@@ -22,8 +22,11 @@ public class ClientDemo01 {
          参数三：服务端的IP地址
          参数四：服务端的端口
          */
-        byte[] buffer = "我是一颗快乐的韭菜，你愿意吃我吗？".getBytes();
+        byte[] buffer = "我是一颗快乐的韭菜，你愿意吃我吗？".getBytes("GBK");
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length, InetAddress.getLocalHost(), 8888);
+
+        // 3. 发送数据出去
+        socket.send(packet);
 
     }
 }
