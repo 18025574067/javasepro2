85e1ac6f4d6c96ac0f8473a3c3d525af498fdfc1
AA <2419805602@qq.com>
2022-09-28 17:26:49 +0800

    123456

---
+++ net-app/src/com/itheima/d8_socket3/ServerDemo02.java
@@ -0,0 +1,32 @@
+package com.itheima.d8_socket3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+    目标：开发Socket网络编程入门代码的服务端，实现接收消息。
+ */
+public class ServerDemo02 {
+    public static void main(String[] args) {
+        System.out.println("==========服务端启动成功===========");
+        try {
+            // 1. 注册端口
+            ServerSocket serverSocket = new ServerSocket(7777);
+
+            // a. 定义一个死循环由主线程负责不断的接收客户端的socket连接请求，
+            while (true){
+                // b. 每接到一个客户端socket管道，都要交给一个子线程负责读取。
+                Socket socket = serverSocket.accept();
+
+                // c. 开始创建独立线程处理socket管道。
+                new ServerReaderThread(socket).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
