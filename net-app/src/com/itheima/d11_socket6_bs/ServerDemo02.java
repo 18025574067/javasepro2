0fb3e980e0e2ca9fd08ed4f231475fc2f9cf5881
AA <2419805602@qq.com>
2022-09-30 16:11:31 +0800

    123456

--- net-app/src/com/itheima/d11_socket6_bs/ServerDemo02.java
+++ net-app/src/com/itheima/d11_socket6_bs/ServerDemo02.java
@@ -3,11 +3,16 @@ package com.itheima.d11_socket6_bs;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.*;
 
 /**
     目标：开发Socket网络编程入门代码的服务端，实现接收消息。
  */
 public class ServerDemo02 {
+    private static ExecutorService pool = new ThreadPoolExecutor(3, 5,
+            6, TimeUnit.SECONDS, new ArrayBlockingQueue<>(2),
+            Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+
     public static void main(String[] args) {
         System.out.println("==========服务端启动成功===========");
         try {
@@ -16,35 +21,10 @@ public class ServerDemo02 {
             while (true){
                 Socket socket = serverSocket.accept();
                 System.out.println(socket.getRemoteSocketAddress() + "上线了");
-                new ServerReaderThread(socket).start();
+                pool.execute(new ServerReaderRunnable(socket));
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-}
-
-
-class ServerReaderThread extends Thread{
-    private Socket socket;
-    public ServerReaderThread(Socket socket){
-        this.socket = socket;
-    }
-    @Override
-    public void run() {
-        try {
-            // 浏览器 已经与本线程建立了Socket管道
-            // 响应消息给浏览器显示
-            PrintStream ps = new PrintStream(socket.getOutputStream());
-            // 必须响应HTTP协议格式数据，否则浏览器不认识消息
-            ps.println("HTTP/1.1 200 OK"); // 协议类型和版本 响应成功的消息！
-            ps.println("Content-Typr:text/html;charset=UTF-8"); // 响应的数据类型：文本/网页
-            ps.println();  // 必须发送一个空行
-            // 才可以响应数据回去给浏览器
-            ps.println("hello world");
-            ps.close();
-        } catch (Exception e) {
-            System.out.println(socket.getRemoteSocketAddress() + "下线了");
         }
     }
 }
