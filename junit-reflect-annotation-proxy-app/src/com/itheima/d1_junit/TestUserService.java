3f90ebd47b850608177445dd50b5c65855369927
hyb2419805602 <2419805602@qq.com>
2022-09-30 21:06:14 +0800

    1234

---
+++ junit-reflect-annotation-proxy-app/src/com/itheima/d1_junit/TestUserService.java
@@ -0,0 +1,17 @@
+package com.itheima.d1_junit;
+
+import org.junit.Test;
+
+/**
+    测试类
+ */
+public class TestUserService {
+    /**
+        测试方法
+        注意点：
+            1. 必须是无参数，公开的，无返回值的方法
+            2. 测试方法必须使用@Test注解标记。
+     */
+    @Test
+    public void testUserService(){}
+}
