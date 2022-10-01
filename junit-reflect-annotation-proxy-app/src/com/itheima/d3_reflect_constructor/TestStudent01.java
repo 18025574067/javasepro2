b42f2e17b938e2b1cf26a0826ae24472d82d0afb
hyb <hyb2419805602@163.com>
2022-10-01 18:46:16 +0800

    12345678

---
+++ junit-reflect-annotation-proxy-app/src/com/itheima/d3_reflect_constructor/TestStudent01.java
@@ -0,0 +1,23 @@
+package com.itheima.d3_reflect_constructor;
+
+/**
+            -- 获取所有申明的构造器，只要你写我就能拿到, 无所谓权限，建议使用！！
+        获取类的某个构造器对象：Constructor getDeclaredCinstructor(Class... parameterTypes)
+            -- 根据参数匹配获取某个构造器，只要申明就可以定位，不关心权限修饰符，建议使用！！
+ */
+
+public class TestStudent01 {
+    // 1. getConstructors;
+    // 获取全部的构造器：只能获取public修饰的构造器。
+    // Construtor[] getConstructors()
+    
+
+
+    // 2. getDec
+
+
+
+
+
+
+}
