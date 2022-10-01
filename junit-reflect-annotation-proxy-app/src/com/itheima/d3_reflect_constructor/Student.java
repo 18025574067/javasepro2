40fdbfb6b3151f7ddefc69a6ff787770c621b0e9
hyb2419805602 <2419805602@qq.com>
2022-10-01 21:28:27 +0800

    1234

--- junit-reflect-annotation-proxy-app/src/com/itheima/d3_reflect_constructor/Student.java
+++ junit-reflect-annotation-proxy-app/src/com/itheima/d3_reflect_constructor/Student.java
@@ -4,7 +4,7 @@ public class Student {
     private String name;
     private int age;
 
-    private Student() { System.out.println("无参数构造执行！");}
+    public Student() { System.out.println("无参数构造执行！");}
 
     private Student(String name, int age) {
         System.out.println("有参数构造器执行");
--- junit-reflect-annotation-proxy-app/src/com/itheima/d3_reflect_constructor/TestStudent01.java
+++ junit-reflect-annotation-proxy-app/src/com/itheima/d3_reflect_constructor/TestStudent01.java
@@ -1,5 +1,9 @@
 package com.itheima.d3_reflect_constructor;
 
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+
 /**
             -- 获取所有申明的构造器，只要你写我就能拿到, 无所谓权限，建议使用！！
         获取类的某个构造器对象：Constructor getDeclaredCinstructor(Class... parameterTypes)
@@ -9,15 +13,39 @@ package com.itheima.d3_reflect_constructor;
 public class TestStudent01 {
     // 1. getConstructors;
     // 获取全部的构造器：只能获取public修饰的构造器。
-    // Construtor[] getConstructors()
+    // Constructor[] getConstructors()
+    @Test
+    public void getConstructors() {
+        // a.第一步：获取类对象
+        Class c = Student.class;
+        // b.提取类中的全部的构造器对象
+        Constructor[] constructors = c.getConstructors();
+        // c.遍历构造器
+        for (Constructor constructor: constructors) {
+            System.out.println(constructor.getName() + "===>" + constructor.getParameterCount());
+        }
+    }
+
+
+    // 2. getDeclaredConstructors():
+    // 获取全部的构造器，只要你敢写，这时就能拿到，无所谓权限是否可及。
+    @Test
+    public void getDeclaredConstructors() {
+        // a.第一步：获取类对象
+        Class c = Student.class;
+        // b.提取类中的全部的构造器对象
+        Constructor[] constructors = c.getDeclaredConstructors();
+        // c.遍历构造器
+        for (Constructor constructor: constructors) {
+            System.out.println(constructor.getName() + "===>" + constructor.getParameterCount());
+        }
+    }   
+
+    // 3. getConstructor(Class...parameterTyes)
+    // 获取某个构造器，只能拿public修饰某个构造器
     
 
-
-    // 2. getDec
-
-
-
-
-
+    // 4. getDeclaredConstructor
+    // 获取某个构造器，只要你敢写，这里就能拿到，无所谓权限是否可及。
 
 }
