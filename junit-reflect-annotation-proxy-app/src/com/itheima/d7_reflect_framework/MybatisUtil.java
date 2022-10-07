a2c26e78bb3bdba4dd984ff1675b0797e050f9f4
hyb2419805602 <2419805602@qq.com>
2022-10-07 23:24:34 +0800

    1234

--- junit-reflect-annotation-proxy-app/src/com/itheima/d7_reflect_framework/MybatisUtil.java
+++ junit-reflect-annotation-proxy-app/src/com/itheima/d7_reflect_framework/MybatisUtil.java
@@ -11,11 +11,10 @@ public class MybatisUtil {
      */
     public static void save(Object obj) {
         try{
-            PrintStream ps = new PrintStream(new FileOutputStream("junit-reflect-proxy-app/src/data.txt", true));
+            PrintStream ps = new PrintStream(new FileOutputStream("junit-reflect-annotation-proxy-app/src/data.txt", true));
             // 1. 提取这个对象的全部成员变量，只有反射可以解决
             Class c = obj.getClass();
-
-
+            ps.println("====================" + c.getSimpleName() + "===================="); // c.getSimpleName()获取当前类名。c.getName()获取全限名：包名+类名。
             // 2. 提取它的全部成员变量
             Field[] fields = c.getDeclaredFields();
             // 3. 获取成员变量的信息
@@ -24,20 +23,11 @@ public class MybatisUtil {
                 // 提取本成员变量在obj对象中的值（取值）
                 field.setAccessible(true);
                 String value = field.get(obj) + "";
-            
-            
+                ps.println(name + "=" + value);
             }
-
-        
-        
-        
+            ps.close();
         } catch (Exception e) {
             e.printStackTrace();
-        
-        
         }
-    
-    
     }
-
 }
--- junit-reflect-annotation-proxy-app/src/com/itheima/d7_reflect_framework/ReflectDemo.java
+++ junit-reflect-annotation-proxy-app/src/com/itheima/d7_reflect_framework/ReflectDemo.java
@@ -5,11 +5,19 @@ package com.itheima.d7_reflect_framework;
  */
 public class ReflectDemo {
     public static void main(String[] args) throws Exception {
-    
-    
-    
-    
-    
-    }
+        Student s = new Student();
+        s.setName("猪八戒");
+        s.setClassName("西天跑路1班");
+        s.setAge(1000);
+        s.setHobby("吃，睡");
+        s.setSex('男');
+        MybatisUtil.save(s);
+ 
+        Teacher t = new Teacher();
+        t.setName("波仔");
+        t.setSex('男');
+        t.setSalary(6000);
+        MybatisUtil.save(t);
 
+    }
 }
