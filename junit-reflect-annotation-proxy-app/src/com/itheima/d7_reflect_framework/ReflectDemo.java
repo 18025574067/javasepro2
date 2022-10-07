feb9434b406edf08185ac502a3c515d29672e404
hyb <hyb2419805602@163.com>
2022-10-07 18:35:35 +0800

    12345678

---
+++ junit-reflect-annotation-proxy-app/src/com/itheima/d7_reflect_framework/ReflectDemo.java
@@ -0,0 +1,15 @@
+package com.itheima.d7_reflect_framework;
+
+/**
+    目标：提供一个通用框架，支持保存所有对象的具体信息
+ */
+public class ReflectDemo {
+    public static void main(String[] args) throws Exception {
+    
+    
+    
+    
+    
+    }
+
+}
