ffb517ad70fc6483bc2532b36cbc072bea039a0f
hyb <hyb2419805602@163.com>
2022-10-07 16:59:30 +0800

    12345678

---
+++ junit-reflect-annotation-proxy-app/src/com/itheima/d6_reflect_genericity/ReflectDemo.java
@@ -0,0 +1,17 @@
+package com.itheima.d6_reflect_genericity;
+
+import java.util.ArrayList;
+
+public class ReflectDemo {
+    public static void main(String[] args) throws Exception {
+        // 需求：反射实现泛型擦除后，加入其他类型的元素
+        ArrayList<Integer> lists = new ArrayList();
+        ArrayList<String> lists1 = new ArrayList();
+        
+
+
+
+    }
+
+
+}
