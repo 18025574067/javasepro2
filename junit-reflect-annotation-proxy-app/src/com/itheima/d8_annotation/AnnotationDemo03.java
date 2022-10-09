83485ed1fa5ff56b45140345a578f30f39bbbf99
hyb2419805602 <2419805602@qq.com>
2022-10-09 23:21:09 +0800

    1234

--- junit-reflect-annotation-proxy-app/src/com/itheima/d8_annotation/AnnotationDemo03.java
+++ junit-reflect-annotation-proxy-app/src/com/itheima/d8_annotation/AnnotationDemo03.java
@@ -2,7 +2,8 @@ package com.itheima.d8_annotation;
 
 
 import org.junit.Test;
-import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
     目标：完成注解的解析。
@@ -15,13 +16,31 @@ public class AnnotationDemo03 {
         // b. 判断这个类上面是否存在这个注解
         if(c.isAnnotationPresent(Bookk.class)){
             // c. 直接获取该注释对象
-            Annotation book = c.getDeclaredAnnotation(Bookk.class);
+            Bookk book = (Bookk) c.getDeclaredAnnotation(Bookk.class);
+            System.out.println(book.value());
+            System.out.println(book.price());
+            System.out.println(Arrays.toString(book.author()));
         }
-    }    
+    }
+
+    @Test
+    public void parseMethod() throws Exception{
+        // a. 先得到类对象
+        Class c = BookStore.class;
+        Method m = c.getDeclaredMethod("test");
+        // b. 判断这个类上面是否存在这个注解
+        if(m.isAnnotationPresent(Bookk.class)){
+            // c. 直接获取该注释对象
+            Bookk book = (Bookk) m.getDeclaredAnnotation(Bookk.class);
+            System.out.println(book.value());
+            System.out.println(book.price());
+            System.out.println(Arrays.toString(book.author()));
+        }
+    }
 }
 
 @Bookk(value="《情深深雨濛濛》", price= 99.9, author={"琼瑶", "dlei"})
 class BookStore{
-    @Bookk(value="《三少爷的剑》", price= 99.9, author={"古龙", "熊耀华"})
+    @Bookk(value="《三少爷的剑》", price= 399.9, author={"古龙", "熊耀华"})
     public void test(){}
 }
