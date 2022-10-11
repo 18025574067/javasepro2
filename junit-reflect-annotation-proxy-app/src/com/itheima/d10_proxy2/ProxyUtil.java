44a77a557912c00d0756ae12f8e025114a65a515
hyb <hyb2419805602@163.com>
2022-10-11 18:43:42 +0800

    12345678

---
+++ junit-reflect-annotation-proxy-app/src/com/itheima/d10_proxy2/ProxyUtil.java
@@ -0,0 +1,6 @@
+package com.itheima.d10_proxy2;
+
+public class ProxyUtil {
+    /** 通过一个静态方法，为用户业务对象返回一个代理对象  */
+
+}
--- junit-reflect-annotation-proxy-app/src/com/itheima/d10_proxy2/UserService.java
+++ junit-reflect-annotation-proxy-app/src/com/itheima/d10_proxy2/UserService.java
@@ -4,6 +4,4 @@ public interface UserService {
     String login(String loginName, String passWord);
     void deleteUsers();
     String selectUsers();
-
-
 }
