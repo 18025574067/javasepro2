2ce6621ea6bc151f50856a6bb6a35b44a6f8b588
hyb <hyb2419805602@163.com>
2022-10-11 17:23:24 +0800

    12345678

--- junit-reflect-annotation-proxy-app/src/com/itheima/d10_proxy2/UserServiceImpl.java
+++ junit-reflect-annotation-proxy-app/src/com/itheima/d10_proxy2/UserServiceImpl.java
@@ -22,6 +22,7 @@ public class UserServiceImpl implements UserService {
     public void deleteUsers(){
         long startTime = System.currentTimeMillis();
         try{
+            System.out.println("您正在删除用户数据中。。。。");
             Thread.sleep(2500);
         } catch (Exception e){
             e.printStackTrace();
@@ -32,6 +33,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public String selectUsers(){
-        return null;
+        long startTime = System.currentTimeMillis();
+        String rs = "查询到了10000个用户数据～～～";
+        try {
+            Thread.sleep(3000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("selectUsers方法耗时：" + (endTime - startTime) / 1000.0 + "s");
+        return rs;
     }
 }
