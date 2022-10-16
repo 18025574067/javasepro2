aaaf0b1193eb486b34631a76d2a15b076a584a2e
hyb2419805602 <2419805602@qq.com>
2022-10-16 23:13:12 +0800

    1234

--- xml-app/src/com/itheima/d2_xpath/XPathDemo.java
+++ xml-app/src/com/itheima/d2_xpath/XPathDemo.java
@@ -17,7 +17,8 @@ public class XPathDemo {
         // a. 创建解析器对象
         SAXReader saxReader = new SAXReader();
         // b. 把XML加载成Document文档对象。
-        Document document = saxReader.read(XPathDemo.class.getResourceAsStream("/Contacts2.xml"));
+        Document document =
+                saxReader.read(XPathDemo.class.getResourceAsStream("/Contacts2.xml"));
         // c. 检索全部的名称
         List<Node> nameNades = document.selectNodes("/contactList/contact/name");
         for (Node nameNade : nameNades) {
@@ -30,7 +31,20 @@ public class XPathDemo {
      2.相对路径： ./子元素/子元素。（.代表了当前元素）
      */
      @Test
-     public void parse02() throws Exception {}
+     public void parse02() throws Exception {
+         // a. 创建解析器对象
+         SAXReader saxReader = new SAXReader();
+         // b. 把XML加载成Document文档对象。
+         Document document =
+                 saxReader.read(XPathDemo.class.getResourceAsStream("/Contacts2.xml"));
+         Element root = document.getRootElement();
+         // c. 检索全部的名称
+         List<Node> nameNades = root.selectNodes("./contact/email");
+         for (Node nameNade : nameNades) {
+             Element nameEle = (Element) nameNade;
+             System.out.println(nameEle.getTextTrim());
+         }
+     }
     
      /**
       3.全文搜索：
@@ -39,7 +53,13 @@ public class XPathDemo {
       // 元素1//元素2 在全文找元素1下面的全部元素2
       */
      @Test
-     public void parse03() throws Exception {}
+     public void parse03() throws Exception {
+         // a. 创建解析器对象
+         SAXReader saxReader = new SAXReader();
+         // b. 把XML加载成Document文档对象。
+         Document document =
+                 saxReader.read(XPathDemo.class.getResourceAsStream("/Contacts2.xml"));
+     }
 
      /**
       4.属性查找
