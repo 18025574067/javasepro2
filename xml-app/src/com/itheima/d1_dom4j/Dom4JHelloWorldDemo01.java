0911737e46bb0c72ad7730bbe6f1ff3f31db06ec
hyb <2419805602@qq.com>
2022-10-22 18:23:47 +0800

    1234

--- xml-app/src/com/itheima/d1_dom4j/Dom4JHelloWorldDemo01.java
+++ xml-app/src/com/itheima/d1_dom4j/Dom4JHelloWorldDemo01.java
@@ -4,7 +4,7 @@ import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 import java.io.InputStream;
 import java.util.List;
